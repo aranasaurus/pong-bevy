a88325178d9647987f24523ecb7eeacaa5765724
Ryan Arana <ryan.arana@hey.com>
2022-02-13 23:45:28 -0800

    Add a size component and a score system

--- src/main.rs
+++ src/main.rs
@@ -35,13 +35,17 @@ fn main() {
                 .with_system(ball_collision),
         )
 
+        .add_system(score)
+
         .run();
 }
 
 #[derive(Component)]
 struct Court {
     top: f32,
-    bottom: f32
+    bottom: f32,
+    left: f32,
+    right: f32
 }
 
 #[derive(Component)]
@@ -62,13 +66,24 @@ struct Ball;
 #[derive(Component)]
 struct Velocity(Vec3);
 
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32
+}
+
 fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     // court
     commands
         .spawn()
-        .insert(Court { top: SCREEN_HEIGHT * 0.5, bottom: -SCREEN_HEIGHT * 0.5 });
+        .insert(Court {
+            top: SCREEN_HEIGHT * 0.5,
+            bottom: -SCREEN_HEIGHT * 0.5,
+            left: -SCREEN_WIDTH * 0.5,
+            right: SCREEN_WIDTH * 0.5,
+        });
 
     // ball
     commands
@@ -81,6 +96,7 @@ fn setup(mut commands: Commands) {
             ..Default::default()
         })
         .insert(Velocity(Vec3::new(BALL_SPEED, BALL_SPEED, 0.0)))
+        .insert(Size { width: BALL_SIZE, height: BALL_SIZE })
         .insert(Ball);
 
     // paddles
@@ -99,7 +115,7 @@ fn setup(mut commands: Commands) {
         binds: Binds { up: KeyCode::W, down: KeyCode::S },
         speed: PADDLE_SPEED,
         score: 0,
-    });
+    }).insert(Size { width: PADDLE_WIDTH, height: PADDLE_HEIGHT });
 
     // player 2
     commands.spawn_bundle(SpriteBundle {
@@ -114,15 +130,15 @@ fn setup(mut commands: Commands) {
         binds: Binds { up: KeyCode::Up, down: KeyCode::Down },
         speed: PADDLE_SPEED,
         score: 0,
-    });
+    }).insert(Size { width: PADDLE_WIDTH, height: PADDLE_HEIGHT });
 }
 
-fn ball_movement(mut ball_q: Query<(&mut Transform, &mut Velocity), With<Ball>>, court_q: Query<&Court>) {
-    let (mut transform, mut velocity) = ball_q.single_mut();
+fn ball_movement(mut ball_q: Query<(&mut Transform, &mut Velocity, &Size), With<Ball>>, court_q: Query<&Court>) {
+    let (mut transform, mut velocity, size) = ball_q.single_mut();
 
     let translation = &mut transform.translation;
     *translation += velocity.0;
-    let radius = BALL_SIZE * 0.5;
+    let radius = size.width * 0.5;
 
     let court = court_q.single();
     if translation.y - radius <= court.bottom {
@@ -132,36 +148,57 @@ fn ball_movement(mut ball_q: Query<(&mut Transform, &mut Velocity), With<Ball>>,
         translation.y = court.top - radius;
         velocity.0.y *= -1.0;
     }
-
-    if translation.x >= SCREEN_WIDTH * 0.5 - radius {
-       velocity.0.x *= -1.0;
-    } else if translation.x <= -SCREEN_WIDTH * 0.5 + radius {
-        velocity.0.x *= -1.0;
-    }
 }
 
-fn ball_collision(mut ball_q: Query<(&mut Transform, &mut Velocity), With<Ball>>, paddle_q: Query<&Transform, (With<Player>, Without<Ball>)>) {
-    let (mut ball_transform, mut velocity) = ball_q.single_mut();
-    for transform in paddle_q.iter() {
-        let horizontal_adjust = (BALL_SIZE * 0.5) + (PADDLE_WIDTH * 0.5);
-        let vertical_adjust = (BALL_SIZE * 0.5) + (PADDLE_HEIGHT * 0.5);
-        while let Some(collision) = collide(ball_transform.translation, Vec2::new(BALL_SIZE, BALL_SIZE), transform.translation, Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)) {
+fn score(mut ball_q: Query<(&Transform, &mut Velocity, &Size), With<Ball>>, mut player_q: Query<(&mut Player, &Transform, &Size)>, court_q: Query<&Court>) {
+    let mut ball = ball_q.single_mut();
+    let ball_x = ball.0.translation.x;
+    let ball_velocity = &mut ball.1.0;
+    let court = court_q.single();
+
+    let scored_right = ball_x - ball.2.width > court.right;
+    let scored_left = ball_x + ball.2.width < court.left;
+
+    if !scored_left && !scored_right {
+        return
+    }
+
+    for (mut player, transform, size) in player_q.iter_mut() {
+        if (transform.translation.x < 0.0) != (ball.0.translation.x < 0.0) {
+            continue
+        }
+
+        player.score += 1;
+        println!("player scored {}", player.score);
+    }
+
+    ball_velocity.x = -ball_velocity.x;
+
+    // TODO: Add events / game states and reset the ball and all that jive. Also show the score. Not here, here should just set the game state or trigger the event or action or whatever approach I end up taking.
+}
+
+fn ball_collision(mut ball_q: Query<(&mut Transform, &mut Velocity, &Size), With<Ball>>, paddle_q: Query<(&Transform, &Size), (With<Player>, Without<Ball>)>) {
+    let (mut ball_transform, mut velocity, ball_size) = ball_q.single_mut();
+    for (paddle_transform, paddle_size) in paddle_q.iter() {
+        let horizontal_adjust = (ball_size.width * 0.5) + (paddle_size.width * 0.5);
+        let vertical_adjust = (ball_size.height * 0.5) + (paddle_size.height * 0.5);
+        if let Some(collision) = collide(ball_transform.translation, Vec2::new(BALL_SIZE, BALL_SIZE), paddle_transform.translation, Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)) {
             match collision {
                 Collision::Left => {
                     velocity.0.x *= -1.0;
-                    ball_transform.translation.x = transform.translation.x - horizontal_adjust;
+                    ball_transform.translation.x = paddle_transform.translation.x - horizontal_adjust;
                 }
                 Collision::Right => {
                     velocity.0.x *= -1.0;
-                    ball_transform.translation.x = transform.translation.x + horizontal_adjust;
+                    ball_transform.translation.x = paddle_transform.translation.x + horizontal_adjust;
                 }
                 Collision::Top => {
-                    velocity.0.y *= -1.0;
-                    ball_transform.translation.y = transform.translation.y + vertical_adjust;
+                    velocity.0.y = velocity.0.y.abs();
+                    ball_transform.translation.y = paddle_transform.translation.y + vertical_adjust;
                 }
                 Collision::Bottom => {
-                    velocity.0.y *= -1.0;
-                    ball_transform.translation.y = transform.translation.y - vertical_adjust;
+                    velocity.0.y = -velocity.0.y.abs();
+                    ball_transform.translation.y = paddle_transform.translation.y - vertical_adjust;
                 }
             }
         }
